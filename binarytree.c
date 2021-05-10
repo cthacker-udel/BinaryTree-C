fd56bbb88003a6e482744762840de5ba1e1786c3
cthacker-udel <cthacker@udel.edu>
2021-05-10 12:06:08 -0400

    Added implementation of making root and also adding nodes onto the root

--- binarytree.c
+++ binarytree.c
@@ -4,6 +4,40 @@
 #include <math.h>
 #include <string.h>
 
+struct node{
+
+    int value;
+    struct node *right;
+    struct node *left;
+
+};
+
+struct node *ROOT = NULL;
+
+void makeRoot(int value){
+
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->value = value;
+    if(ROOT == NULL){
+        ROOT = newNode;
+        newNode->left = NULL;
+        newNode->right = NULL;    
+    }
+}
+
+void makeNode(int value, struct node *parentNode){
+
+    struct node *newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->value = value;
+    newNode->right = NULL;
+    newNode->left = NULL;
+    if(parentNode == NULL){
+        parentNode = newNode;
+    }
+
+}
+
+
 
 int main(){
 
